d955bbd6032823f81538ba4e3151e9279bc3f3ba
legolasse99 <erik.kronkvist99@gmail.com>
2022-11-02 18:25:21 +0100

    Update project0Functions.py

--- Projects/project0Functions.py
+++ Projects/project0Functions.py
@@ -1,7 +1,7 @@
 from cmath import exp
 import re
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import scipy.linalg as linalg
 
 
@@ -11,15 +11,40 @@ def eulerstep(A, uold, h):
 def eulerint(A, y0, t0, tf, N):
     h=(tf-t0)/N
     uold = y0
-    tgrid = np.zeros(shape=(1,N))
+    tgrid = np.zeros(N)
 
     for i in range(N):
         unew = eulerstep(A, uold, h)
         uold = unew
-        tgrid[1,i] = unew
+        tgrid[i] = unew
 
     approx=unew
-    err= approx - linalg.expm(tf*A)
+    err= linalg.norm(approx - linalg.expm(tf*A))
     return [tgrid, approx, err]
 
-[tgrid, approx, err] = eulerint(2, 1, 0, 1, 100)
+def errVSh(A, y0, t0, tf):
+    NArray = np.arange(10,500)
+    errAxis = np.zeros(len(NArray))
+    h = (tf-t0)/NArray
+    for i in range(len(NArray)):
+        [tgrid, approx, err] = eulerint(A, y0, t0, tf, NArray[i])
+        errAxis[i] = err
+    plt.title("Error per h")
+    plt.loglog(h, errAxis)
+    plt.show()
+
+"""
+N = 10000
+[tgrid, approx, err] = eulerint(np.matrix(2), 1, 0, 1, N)
+print("Error: " + str(err))
+
+t = np.linspace(0,1,N)
+realY = np.exp(2*t)
+plt.title("Task 1.2")
+plt.plot(t, tgrid, label = "Approximation")
+plt.plot(t, realY, label = "Real function")
+plt.legend()
+plt.show()
+"""
+
+errVSh(np.matrix(2), 1, 0, 1)
