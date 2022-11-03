245e9a0740907bc816dde7c9ef7e7e67c5ea8b8a
Tobias Möller <tobias@moller.se>
2022-11-03 12:24:49 +0100

    1.2-1.4

--- Projects/project0.py
+++ Projects/project0.py
@@ -1,2 +1,68 @@
-import eulerint
-[tgrid, approx, err] = eulerint(2, 1, 0, 1, 100)
+import project0Functions as lok
+import matplotlib.pyplot as plt
+import numpy as np
+
+"""
+#TASK 1.2 
+N = 100
+[tgrid, approx, err] = eulerint(np.matrix(2), 1, 0, 1, N)
+print("Error: " + str(err))
+
+
+realY = np.exp(2*tgrid)
+plt.title("Task 1.2")
+plt.plot(tgrid, approx, label = "Approximation")
+plt.plot(tgrid, realY, label = "Real function")
+plt.legend()
+plt.show()
+"""
+
+"""
+#TASK 1.3
+
+h1, e1 = lok.errVSh(np.matrix(1), 1, 0, 1)
+h2, e2 = lok.errVSh(np.matrix(2), 1, 0, 1)
+h3, e3 = lok.errVSh(np.matrix(3), 1, 0, 1)
+h4, e4 = lok.errVSh(np.matrix(4), 1, 0, 1)
+h5, e5 = lok.errVSh(np.matrix(5), 1, 0, 1)
+
+plt.title("Error per h")
+plt.loglog(h1, e1, label ="A = 1")
+plt.loglog(h2, e2, label ="A = 2")
+plt.loglog(h3, e3, label ="A = 3")
+plt.loglog(h4, e4, label ="A = 4")
+plt.loglog(h5,e5, label = "A = 5")
+plt.legend()
+plt.show()
+
+# Felet ökar avtagande 
+
+"""
+"""
+# TASK 1.4
+[tgrid, approx, err] = lok.eulerint(np.matrix(3), 1, 0, 1, 1000)
+plt.title("Error per timepoint")
+plt.xlabel("Time")
+plt.ylabel("log(err)")
+plt.semilogy(tgrid, err)
+plt.show()
+"""
+
+# TASK 1.5
+
+A = np.matrix('-1 10; 0 -3')
+y0 = np.matrix('1;1')
+h1, e1 = lok.errVSh(A, y0, 0, 1)
+#h2, e2 = lok.errVSh(np.matrix(2), 1, 0, 1)
+#h3, e3 = lok.errVSh(np.matrix(3), 1, 0, 1)
+#h4, e4 = lok.errVSh(np.matrix(4), 1, 0, 1)
+#h5, e5 = lok.errVSh(np.matrix(5), 1, 0, 1)
+
+plt.title("Error per h")
+plt.loglog(h1, e1, label ="A = 1")
+#plt.loglog(h2, e2, label ="A = 2")
+#plt.loglog(h3, e3, label ="A = 3")
+#plt.loglog(h4, e4, label ="A = 4")
+#plt.loglog(h5,e5, label = "A = 5")
+plt.legend()
+plt.show()
--- Projects/project0Functions.py
+++ Projects/project0Functions.py
@@ -11,40 +11,34 @@ def eulerstep(A, uold, h):
 def eulerint(A, y0, t0, tf, N):
     h=(tf-t0)/N
     uold = y0
-    tgrid = np.zeros(N)
+    unew = y0
+    tgrid = np.arange(t0,tf,1/N)
+    err = np.zeros(N)
+    approx = np.zeros(N)
 
     for i in range(N):
-        unew = eulerstep(A, uold, h)
+        
+        approx[i] = unew
+        err[i] = linalg.norm(unew - linalg.expm(tgrid[i]*A))
         uold = unew
-        tgrid[i] = unew
+        unew = eulerstep(A, uold, h)
+        
+        
 
-    approx=unew
-    err= linalg.norm(approx - linalg.expm(tf*A))
     return [tgrid, approx, err]
 
 def errVSh(A, y0, t0, tf):
-    NArray = np.arange(10,500)
-    errAxis = np.zeros(len(NArray))
+    NArray  = np.arange(10,150)
+    endpointErr = np.zeros(len(NArray))
     h = (tf-t0)/NArray
     for i in range(len(NArray)):
         [tgrid, approx, err] = eulerint(A, y0, t0, tf, NArray[i])
-        errAxis[i] = err
+        endpointErr[i] = err[NArray[i]-1]
+    return h, endpointErr
+    """
     plt.title("Error per h")
-    plt.loglog(h, errAxis)
+    plt.loglog(h, endpointErr)
     plt.show()
+    """
+    
 
-"""
-N = 10000
-[tgrid, approx, err] = eulerint(np.matrix(2), 1, 0, 1, N)
-print("Error: " + str(err))
-
-t = np.linspace(0,1,N)
-realY = np.exp(2*t)
-plt.title("Task 1.2")
-plt.plot(t, tgrid, label = "Approximation")
-plt.plot(t, realY, label = "Real function")
-plt.legend()
-plt.show()
-"""
-
-errVSh(np.matrix(2), 1, 0, 1)
