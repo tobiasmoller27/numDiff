6721d56d4f23ba5f75ebfe988897273c5e40d0c2
Tobias Möller <tobias@moller.se>
2022-11-04 12:24:37 +0100

    Klara fram tills implicit

--- Projects/project0.py
+++ Projects/project0.py
@@ -1,6 +1,7 @@
 import project0Functions as lok
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.linalg as linalg
 
 """
 #TASK 1.2 
@@ -47,22 +48,39 @@ plt.ylabel("log(err)")
 plt.semilogy(tgrid, err)
 plt.show()
 """
-
+"""
 # TASK 1.5
 
 A = np.matrix('-1 10; 0 -3')
+B = np.matrix('1 3 9; 2 3 8; 0 12 7')
 y0 = np.matrix('1;1')
-h1, e1 = lok.errVSh(A, y0, 0, 1)
-#h2, e2 = lok.errVSh(np.matrix(2), 1, 0, 1)
+h1, e1 = lok.errVSh(A, y0, 0, 10)
+h2, e2 = lok.errVSh(A, y0, 0, 10)
 #h3, e3 = lok.errVSh(np.matrix(3), 1, 0, 1)
 #h4, e4 = lok.errVSh(np.matrix(4), 1, 0, 1)
 #h5, e5 = lok.errVSh(np.matrix(5), 1, 0, 1)
 
 plt.title("Error per h")
 plt.loglog(h1, e1, label ="A = 1")
-#plt.loglog(h2, e2, label ="A = 2")
+plt.loglog(h2, e2, label ="A = 2")
 #plt.loglog(h3, e3, label ="A = 3")
 #plt.loglog(h4, e4, label ="A = 4")
 #plt.loglog(h5,e5, label = "A = 5")
 plt.legend()
 plt.show()
+"""
+
+#Task 1.6  Inexplicit euler
+
+N = 100
+[tgrid, approx, err] = lok.ieulerint(np.matrix('-1 100; 0 -30'), np.matrix('1;1'), 0, 1, N)
+print("Error: " + str(err))
+
+
+realY = linalg.expm(tgrid*np.matrix('-1 100; 0 -30'))
+plt.title("Task 1.2")
+plt.plot(tgrid, approx, label = "Approximation")
+plt.plot(tgrid, realY, label = "Real function")
+plt.legend()
+plt.show()
+
--- Projects/project0Functions.py
+++ Projects/project0Functions.py
@@ -14,17 +14,14 @@ def eulerint(A, y0, t0, tf, N):
     unew = y0
     tgrid = np.arange(t0,tf,1/N)
     err = np.zeros(N)
-    approx = np.zeros(N)
+    approx = np.zeros((2,N))
 
     for i in range(N):
-        
-        approx[i] = unew
+        r, c = 0, i
+        approx[r:r+unew.shape[0], c:c+unew.shape[1]] = unew
         err[i] = linalg.norm(unew - linalg.expm(tgrid[i]*A))
         uold = unew
         unew = eulerstep(A, uold, h)
-        
-        
-
     return [tgrid, approx, err]
 
 def errVSh(A, y0, t0, tf):
@@ -40,5 +37,23 @@ def errVSh(A, y0, t0, tf):
     plt.loglog(h, endpointErr)
     plt.show()
     """
-    
+
+def ieulerstep(A, uold, h):
+    return uold + h*A*uold*+(h**2)*(A**2)*uold
+
+def ieulerint(A, y0, t0, tf, N):
+     h=(tf-t0)/N
+     uold = y0
+     unew = y0
+     tgrid = np.arange(t0,tf,1/N)
+     err = np.zeros(N)
+     approx = np.zeros((2,N))
+     for i in range(N):
+        r, c = 0, i
+        approx[r:r+unew.shape[0], c:c+unew.shape[1]] = unew
+        err[i] = linalg.norm(unew - linalg.expm(tgrid[i]*A))
+        uold = unew
+        unew = eulerstep(A, uold, h)
+     return [tgrid, approx, err]
+
 
